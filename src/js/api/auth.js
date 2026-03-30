a9ac5ab1f1fbbb9a23eafc5ed155f660158c732b
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-03-30 06:35:48 +0100

    first commit

---
+++ src/js/api/auth.js
@@ -0,0 +1,34 @@
+import { baseUrl } from '../configs' 
+
+export const login = async (input_data) => {
+    return fetch(baseUrl.backend + '/v1/auth/login.php', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(input_data)
+    })
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => console.error('Login error:', error));
+}
+
+
+
+export const logout = async (session_token) => {
+    return fetch(baseUrl.backend + '/v1/auth/login.php', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(session_token)
+    })
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => console.error('Login error:', error));
+}
+
+export const register = () => {
+    
+}
+
---
+++ src/js/configs.js
@@ -0,0 +1,9 @@
+
+
+export const baseUrl = {
+    backend: 'http://localhost/ADS-Finals---Assignment-1-PHP-MYSQL-security/api',
+    frontend: '/class-scheduler'
+};
+
+
+
