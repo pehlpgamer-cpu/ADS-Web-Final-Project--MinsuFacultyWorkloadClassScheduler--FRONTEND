2f24acf463ca68255fe03386011d515cf8a8f5c1
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-05-01 01:21:18 +0100

    update auth api

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,4 +1,5 @@
-import { baseUrl } from '../configs';
+import { baseUrl, user_role_ids } from '../configs';
+import { safeSetLocalStorage, safeGetLocalStorage } from '../utils/localStorage';
 
 export const login = async (input_data) => {
     const res = await fetch(baseUrl.backend + '/v1/auth/login.php', {
@@ -12,31 +13,35 @@ export const login = async (input_data) => {
     return await res.json();
 };
 
-export const logout = async (session_token) => {
-    return fetch(baseUrl.backend + '/v1/auth/login.php', {
+
+export const logout = async () => {
+    
+    const session_token = safeGetLocalStorage("session_token");
+
+    const res = await fetch(baseUrl.backend + "/v1/auth/logout.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "x-session-token": session_token
+            "x-session-token": session_token ?? ""
         },
-        body: JSON.stringify(session_token)
-    })
-    .then(res => res.json())
-    .then(data => data)
-    .catch(error => console.error('Login error:', error));
-}
+        body: JSON.stringify({
+            session_token: session_token
+        })
+    });
 
+    return await res.json();
+    
+};
 
 export const register = async (input_data) => {
-    return fetch(baseUrl.backend + '/v1/auth/register.php', {
+    
+    const res = await fetch(baseUrl.backend + "/v1/auth/register.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(input_data)
-    })
-    .then(res => res.json())
-    .then(data => data)
-    .catch(error => console.error('Login error:', error));
-}
+    });
 
+    return await res.json();
+};
